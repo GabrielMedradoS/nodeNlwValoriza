53e89213f646ce1650ce867ee6a4e4f3e420a45b
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-01-22 11:31:14 -0300

    Controller compliment

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,14 @@ import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/tags", ensureAdmin, createTagController.handle);
 
@@ -17,7 +19,7 @@ router.use(ensureAdmin);
 para isso n acontecer isso coloco o ensure admin da rota /tags */
 
 router.post("/users", createUserController.handle);
-
 router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export { router };
